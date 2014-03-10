4f31852e55e8daf7c52ee7cc6b95a7a7c43fabac
Roy Prins <prinsroy@gmail.com>
2014-03-10 14:18:38 +0100

    Automated commit

--- commit-both.sh
+++ commit-both.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 read -p "Commit to master, freeze and publish to gh-pages; Commit description: " desc
+find /pdfcache -type f -name '*.pdf' -print0 |  xargs -0 rm
 git add . && \
 git add -u && \
 git commit -m "$desc" && \
-git push heroku master
+git push origin master
+python freeze.py
+cd gh-pages
+gh-pages/git add . && \
+git add -u && \
+git commit -m "$desc" && \
+git push origin gh-pages
+cd ..
