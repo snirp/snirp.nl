0dfaa3474dfea964da59f239783cead4875e552a
Roy Prins <prinsroy@gmail.com>
2014-03-10 14:39:58 +0100

    enhanced automated commit

--- commit-both.sh
+++ commit-both.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-read -p "Commit to master, freeze and publish to gh-pages; Commit description: " desc
+# Make sure we are using the virtualenv
+source venv2/bin/activate
+read -p "Commit description: " desc
+
+# Delete .pdf files in pdfcache
 find /pdfcache -type f -name '*.pdf' -print0 |  xargs -0 rm
+
+# Commit master branch
 git add . && \
 git add -u && \
 git commit -m "$desc" && \
 git push origin master
+
+# Freeze and commit gh-pages branch
 python freeze.py
 cd gh-pages
-gh-pages/git add . && \
+git add . && \
 git add -u && \
 git commit -m "$desc" && \
 git push origin gh-pages
